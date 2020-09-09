5759afa267503be5f6562f757285c64b0537f36c
enzoperce35 <enzoperce35@gmail.com>
2020-09-09 08:23:15 +0800

    finished the assignment

---
+++ fibonacci.rb
@@ -0,0 +1,12 @@
+def fibs(num)
+  arr = [0, 1]
+  num.times { arr << arr[-2] + arr[-1] }
+  arr[0, num + 1]
+end
+
+def fibs_rec(num, arr = [0, 1])
+  arr.count == num + 1 ? arr : fibs_rec(num, arr.push(arr[-2] + arr[-1]))
+end
+
+p fibs 6 #=> [0, 1, 1, 2, 3, 5, 8]
+p fibs_rec 6 #=> [0, 1, 1, 2, 3, 5, 8]
